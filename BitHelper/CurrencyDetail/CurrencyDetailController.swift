6d13b6810d3abd12709f3348d071862f0aa3b127
Берн <m.yakimov@profsoft.pro>
2020-01-03 08:57:16 +0300

    refactoring

--- BitHelper/CurrencyDetail/CurrencyDetailController.swift
+++ BitHelper/CurrencyDetail/CurrencyDetailController.swift
@@ -16,31 +16,26 @@ class CurrencyDetailController: UIViewController, StoryboardBased {
     @IBOutlet weak var symbolLabel: UILabel!
     @IBOutlet weak var priceUSDLabel: UILabel!
     @IBOutlet weak var priceBTCLabel: UILabel!
-    var currency: CryptoCurrency?
     let model = CurrencyDetailModel()
     let disposeBag = DisposeBag()
     
     static func instantiateWith(_ model: CryptoCurrency) -> CurrencyDetailController {
         let controller = CurrencyDetailController.instantiate()
-        controller.currency = model
+        controller.model.currencyContainer.accept([model])
         return controller
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let model = currency {
-            updateUI(model: model)
-        }
 
         model.currencyContainer.asDriver().drive(onNext: { [unowned self] (currencies) in
             if let value = currencies.value, let model = value.first {
-                
-                self.updateUI(model: model)
+                self.updateUI(model)
             }
             }).disposed(by: disposeBag)
     }
     
-    func updateUI(model: CryptoCurrency) {
+    func updateUI(_ model: CryptoCurrency) {
         title = model.name
         symbolLabel.text = model.symbol
         priceUSDLabel.text = model.priceUsd
@@ -48,7 +43,7 @@ class CurrencyDetailController: UIViewController, StoryboardBased {
     }
 
     @IBAction func updatePriceTapped(_ sender: Any) {
-        if let currencyId = currency?.id {
+        if let currencyId = model.currencyContainer.value?.first?.id {
             model.updateCurrency(currencyId)
         }
     }
